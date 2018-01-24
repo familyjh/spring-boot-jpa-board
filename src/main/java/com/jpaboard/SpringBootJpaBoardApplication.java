70ad39bba27585db9d525c41fda0049d9b204349
familyjh <familyjh@gmail.com>
2018-01-24 11:44:23 +0900

    firsh commit

---
+++ src/main/java/com/jpaboard/SpringBootJpaBoardApplication.java
@@ -0,0 +1,12 @@
+package com.jpaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaBoardApplication.class, args);
+	}
+}
