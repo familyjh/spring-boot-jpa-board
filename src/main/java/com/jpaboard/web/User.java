ddede78547a869f60695bcf0324cf67dad096fbd
familyjh <familyjh@gmail.com>
2018-01-25 10:14:45 +0900

    no message

---
+++ src/main/java/com/jpaboard/web/User.java
@@ -0,0 +1,31 @@
+package com.jpaboard.web;
+
+public class User {
+	private String userID;
+	private String userPWD;
+	private String userName;
+	private String userEmail;
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public void setUserPWD(String userPWD) {
+		this.userPWD = userPWD;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userID=" + userID + ", userPWD=" + userPWD + ", userName=" + userName + ", userEmail=" + userEmail+ "]";
+	}
+
+	
+}
